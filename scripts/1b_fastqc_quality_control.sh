3b1c2935b9ead6ece5317c99508f14877d6e5194
Linn Zetterberg Huser <lihu6475@s193.uppmax.uu.se>
2022-03-30 14:31:29 +0200

    Script 1

---
+++ scripts/1b_fastqc_quality_control.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2022-2-5
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 01:00:00
+#SBATCH -J fastqc
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user linn.zetterberg-huser.6475@student.uu.se
+#SBATCH --output=/home/lihu6475/1MB462-PIII/slurm_outs/%j.out
+
+# FastQC quality control script 
+
+# Load modules
+module load bioinfo-tools
+module load FastQC/0.11.9
+
+# Sequence directories 
+SEQDIR="/home/lihu6475/1MB462-PIII/data"
+OUTDIR="/home/lihu6475/1MB462-PIII/analyses/1_fastqc_quality_control"
+
+# Run FastQC trimmed DNA
+for file in $SEQDIR/DNA_trimmed/*
+do
+fastqc $file -o $OUTDIR/
+done
+
+# Run FastQC untrimmed DNA
+for file in $SEQDIR/RNA_untrimmed/*
+do
+fastqc $file -o $OUTDIR/
+done
+
+# Run FastQC on trimmed RNA
+for file in $SEQDIR/RNA_trimmed/*
+do
+fastqc $file -o $OUTDIR/
+done
+
