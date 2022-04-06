16adf2914ae2fedc0ba84b3b2cf669b474f2f73f
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-06 16:36:29 +0200

    Update 1c_multiqc_gather_fastqcs.sh

--- scripts/1c_multiqc_gather_fastqcs.sh
+++ scripts/1c_multiqc_gather_fastqcs.sh
@@ -5,7 +5,7 @@ module load bioinfo-tools
 module load MultiQC
 
 # Sequence directories
-cd /home/lihu6475/1MB462-PIII/analyses/1_fastqc_quality_control
+cd /home/lihu6475/1MB462-PIII/analyses/1_quality_control
 
 # Run MultiQC on all .fastqc in directory
 multiqc .
