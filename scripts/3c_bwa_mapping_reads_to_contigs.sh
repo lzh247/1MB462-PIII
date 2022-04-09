b3efbfb331e83fe31bc3250483eaf820c68d65f3
Linn Zetterberg Huser <lihu6475@rackham2.uppmax.uu.se>
2022-04-09 08:36:22 +0200

    update

--- scripts/3c_bwa_mapping_reads_to_contigs.sh
+++ scripts/3c_bwa_mapping_reads_to_contigs.sh
@@ -21,10 +21,14 @@ OUTDIR="/home/lihu6475/1MB462-PIII/analyses/3_binning_evaluation/binning_with_de
 
 # Run BWA on D1
 bwa index $CONTIGDIR/D1_megahit_output/final.contigs.fa 2>$OUTDIR/D1_index.out
-bwa mem -t 2 $CONTIGDIR/D1_megahit_output/final.contigs.fa $READDIR/SRR4342129_1.paired.trimmed.fastq.gz $READDIR/SRR4342129_2.paired.trimmed.fastq.gz > $OUTDIR/D1_DNA_supercontig.sam 2>$OUTDIR/D1_DNA_bwamem.out
-samtools view -S -b $OUTDIR/D1_DNA_supercontig.sam > $OUTDIR/D1_DNA_supercontig.bam
+#bwa mem -t 12 $CONTIGDIR/D1_megahit_output/final.contigs.fa $READDIR/SRR4342129_1.paired.trimmed.fastq.gz $READDIR/SRR4342129_2.paired.trimmed.fastq.gz > $OUTDIR/D1_DNA_supercontig.sam 2>$OUTDIR/D1_DNA_bwamem.out
+bwa mem -t 12 $CONTIGDIR/D1_megahit_output/final.contigs.fa $READDIR/SRR4342129_1.paired.trimmed.fastq.gz $READDIR/SRR4342129_2.paired.trimmed.fastq.gz 2> $OUTDIR/D1.bam.bwa.log | samtools view -b | samtools sort -o $OUTDIR/D1.sorted.bam -O BAM
+samtools index $OUTDIR/D1.sorted.bam
+#samtools view -S -b -h $OUTDIR/D1_DNA_supercontig.sam > $OUTDIR/D1_DNA_supercontig.bam
 
 # Run BWA on D3
 bwa index $CONTIGDIR/D3_megahit_output/final.contigs.fa 2>$OUTDIR/D3_index.out
-bwa mem -t 2 $CONTIGDIR/D3_megahit_output/final.contigs.fa $READDIR/SRR4342133_1.paired.trimmed.fastq.gz $READDIR/SRR4342133_2.paired.trimmed.fastq.gz > $OUTDIR/D3_DNA_supercontig.sam 2>$OUTDIR/D3_DNA_bwamem.out
-samtools view -S -b $OUTDIR/D3_DNA_supercontig.sam > $OUTDIR/D3_DNA_supercontig.bam
+#bwa mem -t 2 -h $CONTIGDIR/D3_megahit_output/final.contigs.fa $READDIR/SRR4342133_1.paired.trimmed.fastq.gz $READDIR/SRR4342133_2.paired.trimmed.fastq.gz > $OUTDIR/D3_DNA_supercontig.sam 2>$OUTDIR/D3_DNA_bwamem.out
+bwa mem -t 12 $CONTIGDIR/D3_megahit_output/final.contigs.fa $READDIR/SRR4342133_1.paired.trimmed.fastq.gz $READDIR/SRR
+samtools index $OUTDIR/D1.sorted.bam
+#samtools view -S -b -h  $OUTDIR/D3_DNA_supercontig.sam > $OUTDIR/D3_DNA_supercontig.bam
