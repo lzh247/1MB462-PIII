9f29f1bfc40de83d9d7aed19d98891157a2a6ed2
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-19 14:30:30 +0200

    Update 3b_checkm_evaluation.sh

--- scripts/3b_checkm_evaluation.sh
+++ scripts/3b_checkm_evaluation.sh
@@ -2,7 +2,7 @@
 #SBATCH -A uppmax2022-2-5
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 4
 #SBATCH -t 04:00:00
 #SBATCH -J checkm
 #SBATCH --mail-type=ALL
@@ -16,8 +16,17 @@ module load CheckM
 
 # Sequence directories 
 SEQDIR="/home/lihu6475/1MB462-PIII/analyses/3_assembly_evaluation"
-OUTDIR=$SEQDIR/...
+OUTDIR=$SEQDIR/checkm_output
 
-# Run CheckM on ... 
-# Threads = 4
-checkm lineage_wf -t 4 -x fa $SEQDIR/... $OUTDIR
+# Move to wanted working directory and create directories. 
+cd $OUTDIR 
+mkdir D1_checkm
+mkdir D3_checkm
+
+# Download the CheckM data to current directory. As of April 2022, the most recent version is 2015.01.16.
+cp -av $HOME/data/CheckM_data/2015_01_16/* .
+checkm data setRoot $PWD
+
+# Run the analyses with combined workflow: lineage_wf. 
+checkm lineage_wf -t 4 -x fa --reduced_tree $SEQDIR/D1_metabat_output/* $OUTDIR/D1_checkm
+checkm lineage_wf -t 4 -x fa --reduced_tree $SEQDIR/D3_metabat_output/* $OUTDIR/D3_checkm
