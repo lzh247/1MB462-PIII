b89b8830fabc8fa3d868f0189f2a795034b5d7d1
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-21 11:08:31 +0200

    Create 4a_structural_annotation.sh

---
+++ scripts/4a_structural_annotation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2022-2-5
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 10:00:00
+#SBATCH -J prokka
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user linn.zetterberg-huser.6475@student.uu.se
+
+# Structural annotation with Prokka script 
+
+# Load modules 
+module load bioinfo-tools prokka
+
+# Sequence directories
+SEQDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/3_binning_evaluation_with_depth"
+OUTDIR="/home/lihu6475/1MB462-PIII/analyses/4_DNA_annotation/structural_prokka"
+
+# Loop for annotation of all .fa 
+for fa in $SEQDIR/D1/*
+do
+echo $fa
+prokka --metagenome --cpus 2 $fa --outdir $OUTDIR/D1/"$fa"_ann.out
+done
+
+for fb in $SEQDIR/D3/*
+do
+echo $fb
+prokka --metagenome --cpus 2 $fb --outdir $OUTDIR/D3/"$fa"_ann.out
+done
