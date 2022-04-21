37080b0ae1a4d05bee0d4425f29694e001fde40b
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-21 10:17:47 +0200

    Update 3c_bwa_mapping_reads_to_contigs.sh

--- scripts/3c_bwa_mapping_reads_to_contigs.sh
+++ scripts/3c_bwa_mapping_reads_to_contigs.sh
@@ -12,7 +12,7 @@
 
 # Load modules
 module load bioinfo-tools
-module load bwa samtools 
+module load bwa samtools MetaBat
 
 # Sequence directories 
 READDIR="/home/lihu6475/1MB462-PIII/data/DNA_trimmed"
@@ -23,7 +23,7 @@ OUTDIR="/home/lihu6475/1MB462-PIII/analyses/3_binning_evaluation/binning_with_de
 bwa index $CONTIGDIR/D1_megahit_output/final.contigs.fa 2> $OUTDIR/D1/D1_index.out
 #bwa mem -t 12 $CONTIGDIR/D1_megahit_output/final.contigs.fa $READDIR/SRR4342129_1.paired.trimmed.fastq.gz $READDIR/SRR4342129_2.paired.trimmed.fastq.gz > $OUTDIR/D1_DNA_supercontig.sam 2>$OUTDIR/D1_DNA_bwamem.out
 #bwa mem -t 12 $CONTIGDIR/D1_megahit_output/final.contigs.fa $READDIR/SRR4342129_1.paired.trimmed.fastq.gz $READDIR/SRR4342129_2.paired.trimmed.fastq.gz 2> $OUTDIR/D1.bam.bwa.log | samtools view -b | samtools sort -o $OUTDIR/D1.sorted.bam -O BAM
-bwa mem -t 12 $CONTIGDIR/D1_megahit_output/final.contigs.fa $READDIR/SRR4342129_1.paired.trimmed.fastq.gz $READDIR/SRR4342129_2.paired.trimmed.fastq.gz 2> $OUTDIR/D1/D1.bam.bwa.log | samtools sort -o $OUTDIR/D1/D1.sorted.bam
+bwa mem $CONTIGDIR/D1_megahit_output/final.contigs.fa $READDIR/SRR4342129_1.paired.trimmed.fastq.gz $READDIR/SRR4342129_2.paired.trimmed.fastq.gz 2> $OUTDIR/D1/D1.bam.bwa.log | samtools sort -o $OUTDIR/D1/D1.sorted.bam
 # Create depth file
 jgi_summarize_bam_contig_depths --outputDepth $OUTDIR/D1/D1_depth.txt $OUTDIR/D1/*.bam
 #samtools index $OUTDIR/D1.sorted.bam
@@ -33,7 +33,7 @@ jgi_summarize_bam_contig_depths --outputDepth $OUTDIR/D1/D1_depth.txt $OUTDIR/D1
 bwa index $CONTIGDIR/D3_megahit_output/final.contigs.fa 2> $OUTDIR/D3/D3_index.out
 #bwa mem -t 2 -h $CONTIGDIR/D3_megahit_output/final.contigs.fa $READDIR/SRR4342133_1.paired.trimmed.fastq.gz $READDIR/SRR4342133_2.paired.trimmed.fastq.gz > $OUTDIR/D3_DNA_supercontig.sam 2>$OUTDIR/D3_DNA_bwamem.out
 #bwa mem -t 12 $CONTIGDIR/D3_megahit_output/final.contigs.fa $READDIR/SRR4342133_1.paired.trimmed.fastq.gz $READDIR/SRR4342133_2.paired.trimmed.fastq.gz 2> $OUTDIR/D3.bam.bwa.log | samtools view -b | samtools sort -o $OUTDIR/D3.sorted.bam -O BAM
-bwa mem -t 12 $CONTIGDIR/D3_megahit_output/final.contigs.fa $READDIR/SRR4342133_1.paired.trimmed.fastq.gz $READDIR/SRR4342133_2.paired.trimmed.fastq.gz 2> $OUTDIR/D3/D3.bam.bwa.log | samtools sort -o $OUTDIR/D3/D3.sorted.bam
+bwa mem $CONTIGDIR/D3_megahit_output/final.contigs.fa $READDIR/SRR4342133_1.paired.trimmed.fastq.gz $READDIR/SRR4342133_2.paired.trimmed.fastq.gz 2> $OUTDIR/D3/D3.bam.bwa.log | samtools sort -o $OUTDIR/D3/D3.sorted.bam
 # Create depth file
 jgi_summarize_bam_contig_depths --outputDepth $OUTDIR/D3/D3_depth.txt $OUTDIR/D3/*.bam
 #samtools index $OUTDIR/D3.sorted.bam
