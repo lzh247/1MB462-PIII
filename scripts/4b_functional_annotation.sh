d1aa4fc13a3af6e60dee765f5feea626799e355b
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-21 11:20:45 +0200

    Create 4b_functional_annotation.sh

---
+++ scripts/4b_functional_annotation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2022-2-5
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 100:00:00
+#SBATCH -J eggnoggmapper
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user linn.zetterberg-huser.6475@student.uu.se
+
+# Functional annotation with EggNoggMapper script
+
+# Load modules
+module load bioinfo-tools eggNOG-mapper
+
+# Sequence directories
+SEQDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/3_binning_evaluation_with_depth"
+OUTDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/4_DNA_annotation/functional_eggnoggmapper"
+
+# Loop
+for fa in $SEQDIR/D1/*
+do
+echo $fa
+emapper.py --itype metagenome -i $fa -o $fa --output_dir $OUTDIR/D1
+done
+
+for fa in $SEQDIR/D3/*
+do
+echo $fa
+emapper.py --itype metagenome -i $fa -o $fa --output_dir $OUTDIR/D3
+done
