fbb924610f594cd8abd49fe6f683654c1e795494
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-21 09:26:41 +0200

    Update 3b_checkm_evaluation.sh

--- scripts/3b_checkm_evaluation.sh
+++ scripts/3b_checkm_evaluation.sh
@@ -29,7 +29,7 @@ cd $OUTDIR
 #checkm lineage_wf -t 4 -x fa --reduced_tree $SEQDIR/D1_metabat_output/ $OUTDIR/D1_checkm
 #checkm lineage_wf -t 4 -x fa --reduced_tree $SEQDIR/D3_metabat_output/ $OUTDIR/D3_checkm
 
-checkm analyze -t 4 -x fa $OUTDIR/D1_checkm/lineage.ms $OUTDIR/D1_checkm/bins $OUTDIR/D1_checkm
-checkm analyze -t 4 -x fa $OUTDIR/D3_checkm/lineage.ms $OUTDIR/D3_checkm/bins $OUTDIR/D3_checkm
+checkm analyze -t 4 -x fa $OUTDIR/D1_checkm/lineage.ms $SEQDIR/D1_metabat_output $OUTDIR/D1_checkm
+checkm analyze -t 4 -x fa $OUTDIR/D3_checkm/lineage.ms $SEQDIR/D3_metabat_output $OUTDIR/D3_checkm
 
 #checkm qa -t 4 -o 1 -f $SNIC_TMP/$A/result/qa.out $SNIC_TMP/$A/result/lineage.ms $SNIC_TMP/$A/result
