d1d751ece97ec879c8572b9679426dd17eea1fa3
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-22 06:49:45 +0200

    Update 3d_metabat_binning_depth.sh

--- scripts/3d_metabat_binning_depth.sh
+++ scripts/3d_metabat_binning_depth.sh
@@ -20,7 +20,9 @@ OUTDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/3_binning_evaluation_wit
 
 # Run MetaBat with depth.txt on D1 and D3
 jgi_summarize_bam_contig_depths --outputDepth $OUTDIR/D1/depth.txt --pairedContigs $OUTDIR/D1/paired.txt $OUTDIR/D1/*.bam 
-metabat -i $CONTIGDIR/D1_megahit_output/final.contigs.fa -a $OUTDIR/D1/depth.txt -p $OUTDIR/D1/paired.txt --specific -v --saveTNF $OUTDIR/D1/saved.tnf --saveDistance $OUTDIR/D1/saved.gprob -o $OUTDIR/D1/D1_normalized
+metabat -i $CONTIGDIR/D1_megahit_output/final.contigs.fa -a $OUTDIR/D1/depth.txt -p $OUTDIR/D1/paired.txt --specific -v --saveTNF $OUTDIR/D1/saved.tnf --saveDistance $OUTDIR/D1/saved.gprob -o $OUTDIR/metabat_output
+/D1/D1_normalized
 
 jgi_summarize_bam_contig_depths --outputDepth $OUTDIR/D3/depth.txt --pairedContigs $OUTDIR/D3/paired.txt $OUTDIR/D3/*.bam
-metabat -i $CONTIGDIR/D3_megahit_output/final.contigs.fa -a $OUTDIR/D3/depth.txt -p $OUTDIR/D3/paired.txt --specific -v --saveTNF $OUTDIR/D3/saved.tnf --saveDistance $OUTDIR/D3/saved.gprob -o $OUTDIR/D3/D3_normalized
+metabat -i $CONTIGDIR/D3_megahit_output/final.contigs.fa -a $OUTDIR/D3/depth.txt -p $OUTDIR/D3/paired.txt --specific -v --saveTNF $OUTDIR/D3/saved.tnf --saveDistance $OUTDIR/D3/saved.gprob -o $OUTDIR/metabat_output
+/D3/D3_normalized
