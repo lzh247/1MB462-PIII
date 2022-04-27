d97b47cd4da75ecb2e573b97d218f69a69163864
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-27 16:21:38 +0200

    Update 5a_bwa_RNA_mapping.sh

--- scripts/5a_bwa_RNA_mapping.sh
+++ scripts/5a_bwa_RNA_mapping.sh
@@ -20,25 +20,21 @@ OUTDIR="/home/lihu6475/1MB462-PIII/analyses/4_DNA_annotation/structural_prokka"
 
 # Combine all .fa bin files into one aggregated file. See article. 
 # D1
-for bin in $BINDIR/D1/*.fa;
-do
-name=`echo $bin | awk 'BEGIN{FS="."}{print $1$3}'`; echo ">"$name >> $OUTDIR/D1_bins_concat.fa; grep -v '>' $bin >> $OUTDIR/D1_bins_concat.fa; 
-done
+#for bin in $BINDIR/D1/*.fa;
+#do
+#name=`echo $bin | awk 'BEGIN{FS="."}{print $1$3}'`; echo ">"$name >> $OUTDIR/D1_bins_concat.fa; grep -v '>' $bin >> $OUTDIR/D1_bins_concat.fa; 
+#done
 
 # D3
-for bin in $BINDIR/D3/*.fa;
-do
-name=`echo $bin | awk 'BEGIN{FS="."}{print $1$3}'`; echo ">"$name >> $OUTDIR/D3_bins_concat.fa; grep -v '>' $bin >> $OUTDIR/D3_bins_concat.fa; 
-done
+#for bin in $BINDIR/D3/*.fa;
+#do
+#name=`echo $bin | awk 'BEGIN{FS="."}{print $1$3}'`; echo ">"$name >> $OUTDIR/D3_bins_concat.fa; grep -v '>' $bin >> $OUTDIR/D3_bins_concat.fa; 
+#done
 
 # Create reference index prior to bwa index
-bwa index $OUTDIR/D1_bins_concat.fa 2> $OUTDIR/D1_index.out
-bwa index $OUTDIR/D3_bins_concat.fa 2> $OUTDIR/D3_index.out
+#bwa index $OUTDIR/D1_bins_concat.fa 2> $OUTDIR/D1_index.out
+#bwa index $OUTDIR/D3_bins_concat.fa 2> $OUTDIR/D3_index.out
 
 # Run bwa mem for alignment
 bwa mem $OUTDIR/D1_bins_concat.fa $SEQDIR/RNA_trim_37.left_paired.trimmed.fastq.gz $SEQDIR/RNA_trim_37.right_paired.trimmed.fastq.gz 2> $OUTDIR/D1_mem.out | samtools sort -o $OUTDIR/D1_sorted.bam
 bwa mem $OUTDIR/D3_bins_concat.fa $SEQDIR/RNA_trim_39.left_paired.trimmed.fastq.gz $SEQDIR/RNA_trim_39.right_paired.trimmed.fastq.gz 2> $OUTDIR/D3_mem.out | samtools sort -o $OUTDIR/D3_sorted.bam
-
-
-
-
