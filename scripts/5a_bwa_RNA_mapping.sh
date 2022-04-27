8f7cd4b21eb968efc6788a87b7fd549d10fb420d
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-27 13:46:25 +0200

    Update 5a_bwa_RNA_mapping.sh

--- scripts/5a_bwa_RNA_mapping.sh
+++ scripts/5a_bwa_RNA_mapping.sh
@@ -13,8 +13,13 @@
 # Load modules 
 module load bioinfo-tools bwa samtools
 
-... 
-
 # Sequence directories
 SEQDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/3_binning_evaluation_with_depth"
 OUTDIR="/home/lihu6475/1MB462-PIII/analyses/4_DNA_annotation/structural_prokka"
+
+# Combine all .fa bin files into one aggregated file. See article. 
+# D1
+for bin in $SEQDIR/*.fa;
+do
+name=`echo $bin | awk 'BEGIN{FS="."}{print $1$3}'`; echo ">"$name >> $OUTDIR/D1_bins_concat.fa; grep -v '>' $bin >> $OUTDIR/D1_bins_concat.fa; 
+done
