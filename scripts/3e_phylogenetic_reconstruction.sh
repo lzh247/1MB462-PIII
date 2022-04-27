245f7212bd8588499c15cd9bc889058a6ce7ffe9
LZH <92080423+lzh247@users.noreply.github.com>
2022-04-27 14:26:13 +0200

    Update 3e_phylogenetic_reconstruction.sh

--- scripts/3e_phylogenetic_reconstruction.sh
+++ scripts/3e_phylogenetic_reconstruction.sh
@@ -12,4 +12,16 @@
 # Phylogenetic reconstruction script, based on bins. 
 
 # Load modules
-module load bioinfo-tools python biopython FasTree muscle usearch/5.2.32
+module load bioinfo-tools phylophlan/0.99 python/2.7.15 biopython/1.7.3 FastTree muscle/3.8.31 usearch/5.2.32
+
+# Sequence rectories
+SEQDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/4_DNA_annotation/structural_prokka"
+OUTDIR="/home/lihu6475/1MB462-PIII/analyses/3b_phylo_reconstruct"
+
+# Copy installed phylophlan files into working directory. 
+cp -r /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/* $OUTDIR/
+
+# Run pylophlan 
+# The input .fa files will be the Prokka output, as these include amino acid sequence and not only nucleotide, in contrast to the binning output.
+$OUTDIR/phylophlan.py -i $SEQDIR/D1/*.fa_ann.out -t --nproc 2 -o $OUTDIR/D1/ 2> D1_phylo.err
+$OUTDIR/phylophlan.py -i $SEQDIR/D3/*.fa_ann.out -t --nproc 2 -o $OUTDIR/D3/ 2> D3_phylo.err
