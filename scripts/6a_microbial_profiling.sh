4d66e1243a0f93b9388fb0b4df44c14ad0588f63
LZH <92080423+lzh247@users.noreply.github.com>
2022-05-11 15:59:39 +0200

    Update 6a_microbial_profiling.sh

--- scripts/6a_microbial_profiling.sh
+++ scripts/6a_microbial_profiling.sh
@@ -19,14 +19,12 @@ module load BioBakery
 #module load python 
 #module load biopython FastTree muscle usearch
 
-dbdir=$SNIC_TMP/db
-mkdir $metadir 
-
-metaphlan --install --bowtie2db $metadir
 
 # Path to sequence directory and output directories
 SEQDIR="/home/lihu6475/1MB462-PIII/data/DNA_trimmed"
 OUTDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/6_extra_analyses/metaphlan"
+
+metaphlan --install --bowtie2db $OUTDIR/install
 
 # Run MetaPhlAn
 #metaphlan $SEQDIR/SRR4342129_1.paired.trimmed.fastq.gz,$SEQDIR/SRR4342129_2.paired.trimmed.fastq.gz --bowtie2db $metadir --bowtie2out $OUTDIR/bt2out.txt --input_type fastq --nproc 2 --input_type fastq --add_viruses -o $OUTDIR/metagenome_profile.txt
