e619543adc1c175fa9841c53f8250a365033104b
LZH <92080423+lzh247@users.noreply.github.com>
2022-05-11 14:19:31 +0200

    Update 5c_diff_expression.sh

--- scripts/5c_diff_expression.sh
+++ scripts/5c_diff_expression.sh
@@ -3,7 +3,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -J diff_expression
-#SBATCH -t 10:00:00
+#SBATCH -t 48:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user linn.zetterberg-huser.6475@student.uu.se
 
@@ -19,13 +19,24 @@ OUTDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/5_mapping_RNA/htseq_out"
 BAMDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/5_mapping_RNA"
 
 # Run htseq
-cd $SEQDIR
-for n in D1/*;
+cd $SEQDIR/D1/gff_collected
+for gff in *.gff
 do
-    for m in 1 2 3 4 5 6 7 8 9 10 11 12;
-    do
-        sed '/##FASTA/,$d' D1/${n}/D1_metabat_output_${m}.fa_ann.out.gff > $OUTDIR/htseq_${m}.gff
-        htseq-count -f bam -t CDS -i ID -s no $BAMDIR/D1_sorted.bam $OUTDIR/htseq_${m}.gff > $OUTDIR/htseq_${m}_D1.counts
-        htseq-count -f bam -t CDS -i ID -s no $BAMDIR/D3_sorted.bam $OUTDIR/htseq_${m}.gff > $OUTDIR/htseq_${m}_D3.counts
-    done
-done
+echo $gff
+intrm=$(echo $gff | cut -f3 -d ".")
+bin=$(echo $intrm | cut -f4 -d "_")
+echo $bin
+cd $OUTDIR
+cd $bin
+htseq-count -f bam -t CDS -r pos -i ID *.bam $SEQDIR/D1/gff_collected/$gff > $OUTDIR/D1_htseq_${bin}.counts
+
+cd $SEQDIR/D3/gff_collected
+for gff in *.gff
+do
+echo $gff
+intrm=$(echo $gff | cut -f3 -d ".")
+bin=$(echo $intrm | cut -f4 -d "_")
+echo $bin
+cd $OUTDIR
+cd $bin
+htseq-count -f bam -t CDS -r pos -i ID *.bam $SEQDIR/D1/gff_collected/$gff > $OUTDIR/D1_htseq_${bin}.counts
