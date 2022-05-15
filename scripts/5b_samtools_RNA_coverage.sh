0936cb96de8e8b940dde672d54a59c1fb1c4074f
LZH <92080423+lzh247@users.noreply.github.com>
2022-05-15 11:18:50 +0200

    Update 5b_samtools_RNA_coverage.sh

--- scripts/5b_samtools_RNA_coverage.sh
+++ scripts/5b_samtools_RNA_coverage.sh
@@ -10,7 +10,8 @@
 # Reading RNA coverage with samtools script 
 
 # Load modules 
-module load bioinfo-tools samtools
+module load bioinfo-tools 
+module load samtools
 
 # Sequence directories
 SEQDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/5_mapping_RNA"
@@ -21,11 +22,11 @@ samtools index $SEQDIR/D1_sorted.bam
 samtools index $SEQDIR/D3_sorted.bam
 
 # Create summary table with statistics 
-samtools coverage $SEQDIR/D1_sorted.bam -o $SEQDIR/D1_coverage
-samtools coverage $SEQDIR/D3_sorted.bam -o $SEQDIR/D3_coverage
+samtools coverage $SEQDIR/D1_sorted.bam -o $OUTDIR/D1_coverage
+samtools coverage $SEQDIR/D3_sorted.bam -o $OUTDIR/D3_coverage
 
 # Depth for per base coverage instead of average
-samtools depth $SEQDIR/D1_sorted.bam -o $SEQDIR/D1_depth  
-samtools depth $SEQDIR/D3_sorted.bam -o $SEQDIR/D3_depth  
+samtools depth $SEQDIR/D1_sorted.bam -o $OUTDIR/D1_depth  
+samtools depth $SEQDIR/D3_sorted.bam -o $OUTDIR/D3_depth  
 
 # More statistics 
