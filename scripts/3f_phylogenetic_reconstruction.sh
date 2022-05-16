7fc9d1d377688b56b9414cbd3aa17c049cb6c9cb
LZH <92080423+lzh247@users.noreply.github.com>
2022-05-16 21:17:47 +0200

    Update 3f_phylogenetic_reconstruction.sh

--- scripts/3f_phylogenetic_reconstruction.sh
+++ scripts/3f_phylogenetic_reconstruction.sh
@@ -10,7 +10,8 @@
 # Phylogenetic reconstruction script, based on bins. 
 
 # Load modules
-module load bioinfo-tools phylophlan/0.99 python/2.7.15 biopython/1.73 FastTree muscle/3.8.31 usearch/5.2.32
+module load bioinfo-tools 
+module load phylophlan/0.99 python/2.7.15 biopython/1.73 FastTree muscle/3.8.31 usearch/5.2.32
 
 # Sequence directories
 SEQDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/4_DNA_annotation/structural_prokka"
@@ -21,41 +22,41 @@ OUTDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/3b_phylo_reconstruct"
 
 # Symlink the phylophlan data files
 cd  $OUTDIR/
-mkdir data
+#mkdir data
 cd data
-mkdir -p ppaalns
-ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/data/*.bz2 .
-ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/data/ppaalns/*.bz2 ppaalns/
-ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/data/ppafull.tax.txt .
+#mkdir -p ppaalns
+#ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/data/*.bz2 .
+#ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/data/ppaalns/*.bz2 ppaalns/
+#ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/data/ppafull.tax.txt .
 ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/taxcuration/ /proj/genomeanalysis2022/nobackup/work/lihu6475/3b_phylo_reconstruct/taxcuration
 
 # The input .fa files will be the Prokka output, as these include amino acid sequence and not only nucleotide, in contrast to the binning output. 
 # Further, the file names have to be given new names.
-cd $SEQDIR/D1
-for bins in *ann.out
-do
-echo $bins
-for files in $bins/*
-do
-echo $files
-mv "$files" "${files/PROKKA_05162022/$bins}"
-done
-done
+#cd $SEQDIR/D1
+#for bins in *ann.out
+#do
+#echo $bins
+#for files in $bins/*
+#do
+#echo $files
+#mv "$files" "${files/PROKKA_05162022/$bins}"
+#done
+#done
 
-cd $SEQDIR/D3
-for bins in *ann.out
-do
-echo $bins
-for files in $bins/*
-do
-echo $files
-mv "$files" "${files/PROKKA_05162022/$bins}"
-done
-done
+#cd $SEQDIR/D3
+#for bins in *ann.out
+#do
+#echo $bins
+#for files in $bins/*
+#do
+#echo $files
+#mv "$files" "${files/PROKKA_05162022/$bins}"
+#done
+#done
 
 # Soft link the input data to phylophlans input directory
-ln -s $SEQDIR/D1/*ann.out/*.faa $OUTDIR/input/D1/
-ln -s $SEQDIR/D3/*ann.out/*.faa $OUTDIR/input/D3/
+#ln -s $SEQDIR/D1/*ann.out/*.faa $OUTDIR/input/D1/
+#ln -s $SEQDIR/D3/*ann.out/*.faa $OUTDIR/input/D3/
 
 # Run pylophlan 
 cd $OUTDIR
