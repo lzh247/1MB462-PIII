a9f7def869bb5ef82f34f12f7753703b606c9547
LZH <92080423+lzh247@users.noreply.github.com>
2022-05-19 14:05:59 +0200

    Update 5a_bwa_RNA_mapping.sh

--- scripts/5a_bwa_RNA_mapping.sh
+++ scripts/5a_bwa_RNA_mapping.sh
@@ -19,6 +19,9 @@ BINDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/3_binning_evaluation_wit
 SEQDIR="/home/lihu6475/1MB462-PIII/data/RNA_trimmed"
 OUTDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/5_mapping_RNA"
 
+GENDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/4_DNA_annotation/structural_prokka"
+GENOUTDIR="/proj/genomeanalysis2022/nobackup/work/lihu6475/5b_bwa_genes"
+
 # D1
 cd $BINDIR/D1
 for fa in D1_normalized.*.fa
@@ -38,3 +41,58 @@ bwa index $OUTDIR/${fa} 2> $OUTDIR/${fa}_index.out
 cd $OUTDIR
 bwa mem $OUTDIR/${fa} $SEQDIR/RNA_trim_37.left_paired.trimmed.fastq.gz $SEQDIR/RNA_trim_37.right_paired.trimmed.fastq.gz 2> $OUTDIR/${fa}_mem.out | samtools sort -o $OUTDIR/${fa}_sorted.bam
 done 
+
+
+# On gene data 
+#sample D1
+cd $GENDIR/D1/
+for bin in *.out
+do
+cd $bin
+for file in *.ffn
+bwa index $file 
+bwa mem -t 10 $file $SEQDIR/RNA_trim_37.left_paired.trimmed.fastq.gz $SEQDIR/RNA_trim_37.right_paired.trimmed.fastq.gz 2>$GENOUTDIR/D1/${file}.sam | samtools sort -o $GENOUTDIR/D1/${file}_sorted.bam
+done
+#sample D3
+cd $GENDIR/D3/
+for bin in *.out
+do
+cd $bin
+for file in *.ffn
+bwa index $file 
+bwa mem -t 10 $file $SEQDIR/RNA_trim_37.left_paired.trimmed.fastq.gz $SEQDIR/RNA_trim_37.right_paired.trimmed.fastq.gz 2>$GENOUTDIR/D3/${file}.sam | samtools sort -o $GENOUTDIR/D3/${file}_sorted.bam
+done
+#sample D1
+cd $GENDIR/D1/
+for bin in *.out
+do
+for file in $GENDIR/D1/$bin/*.ffn
+do
+basename_file=$(basename -s .ffn $file)
+bwa index $file
+bwa mem -t 10 $file $SEQDIR/RNA_trim_39.left_paired.trimmed.fastq.gz $SEQDIR/RNA_trim_39.right_paired.trimmed.fastq.gz 2>$GENOUTDIR/D1/${basename_file}.sam | samtools sort -o $GENOUTDIR/D1/${basename_file}_sorted.bam
+done
+done
+#sample D3
+cd $GENDIR/D3/
+for bin in *.out
+do
+for file in $GENDIR/D3/$bin/*.ffn
+do
+basename_file=$(basename -s .ffn $file)
+bwa index $file
+bwa mem -t 10 $file $SEQDIR/RNA_trim_39.left_paired.trimmed.fastq.gz $SEQDIR/RNA_trim_39.right_paired.trimmed.fastq.gz 2>$GENOUTDIR/D3/${basename_file}.sam | samtools sort -o $GENOUTDIR/D3/${basename_file}_sorted.bam
+done
+done
+
+cd $GENOUTDIR/D1
+for bam in D1_normalized.*.fa_ann.out_sorted.bam
+do 
+samtools stats $bam | grep ^SN | cut -f 2- > $GENOUTDIR/D1/${bam}_summary  
+done
+
+cd $GENOUTDIR/D3
+for bam in D3_normalized.*.fa_ann.out_sorted.bam
+do 
+samtools stats $bam | grep ^SN | cut -f 2- > $GENOUTDIR/D3/${bam}_summary  
+done
